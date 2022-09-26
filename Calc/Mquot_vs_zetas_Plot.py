0d3d3cb7d85f0d8098a82a04e1342bbdd75a967d
HateFindingNames <52651426+HateFindingNames@users.noreply.github.com>
2022-09-26 19:10:44 +0200

    Final massage continues

--- Calc/Mquot_vs_zetas_Plot.py
+++ Calc/Mquot_vs_zetas_Plot.py
@@ -28,11 +28,11 @@ zetas = np.arange(1,3,.1)
 mfontsize = 14
 plotsize = .5
 fig, TTh = plt.subplots(nrows=1, ncols=1, figsize=(plotsize * 18, plotsize * 9))
-TTh.plot(zetas, ((T*zetas)/Th), color="black", ls="solid", linewidth=.5, label="$\\frac{T}{T_{Hang}}$")
+TTh.plot(zetas, ((T*zetas)/Th), color="black", ls="solid", linewidth=.5, label="$\\frac{M}{M_{Hang}}$")
 vmax = TTh.twinx()
 vmax.plot(zetas, ((rpm/zetas)*np.pi*2*r*(60/1000)), color="black", ls="--", linewidth=.5, label="$v_{max}$")
 TTh.set_xlabel("$\\zeta$ / 1", fontsize=mfontsize-2)
-TTh.set_ylabel("$\\frac{T}{T_{Hang}}$ / $N \\, m$", fontsize=mfontsize-2)
+TTh.set_ylabel("$\\frac{M}{M_{Hang}}$ / $N \\, m$", fontsize=mfontsize-2)
 vmax.set_ylabel("$v_{max}$ / $km \\, h^{-1}$", fontsize=mfontsize-2)
 TTh.set_title("Drehmomentenquotient und Maximalgeschwindigkeit gegen $\\zeta$", fontsize=mfontsize+2)
 
@@ -45,5 +45,5 @@ lines2, labels2 = vmax.get_legend_handles_labels()
 vmax.legend(lines + lines2, labels + labels2, loc=7, fontsize=mfontsize-4)
 
 plt.tight_layout()
-plt.savefig("Calc\\T_v_vs_zetas.svg", transparent=True)
+plt.savefig("Calc\\Mquot_vs_zetas.svg", transparent=True)
 # plt.show()
